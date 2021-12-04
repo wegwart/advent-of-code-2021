7b4daea3f805cb5cbbe89569dd3073fca54203c3
Robert Wegwart <robert.wegwart@gmail.com>
2021-12-04 17:39:58 +0100

    Day 3: Binary Diagnostic

---
+++ day2_part2.py
@@ -0,0 +1,29 @@
+
+def day2(file_name):
+    with open(file_name) as f:
+        horizontal = 0
+        depth = 0
+        aim = 0
+        for index, line in enumerate(f):
+            splitted_line = line.split(' ')
+            direction = splitted_line[0]
+            distance = int(splitted_line[1])
+            if direction == 'forward':
+                horizontal += distance
+                depth += aim * distance 
+                print('forward {} adds {} to your horizontal position, a total of {}. Because'.format(distance, distance, horizontal))
+                if aim == 0:
+                    print('your aim is 0, your depth does not change.')
+                else:
+                    print('your aim is {}, your depth increases by {}*{}={} to a total of {}.'.format(aim, aim, distance, aim * distance, depth))
+            elif direction == "down":
+                aim += distance
+                print('down {} adds {} to your aim, resulting in a value of {}.'.format(distance, distance, aim))
+            elif direction == "up":
+                aim -= distance
+                print('up {} adds {} to your aim, resulting in a value of {}.'.format(distance, distance, aim))
+        print("{} * {} => {}".format(horizontal, depth, horizontal * depth))
+
+if __name__ == '__main__':
+    day2('day2_test.txt')
+    day2('day2_puzzle.txt')
